5afbf67d2377ff9b23f881665cbd8519b593567a
Tossah Fafa <fafatossah@gmail.com>
2026-08-26 15:58:40 +0000

    Association des images aux ressource texte(String.xml et ContenteDescription)

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -9,6 +9,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lemonade.ui.theme.LemonadeTheme
@@ -20,10 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LemonadeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LemonadeApp()
                 }
             }
         }
@@ -31,11 +32,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LemonadeApp() {
+
+    var etapeActuelle by remember { mutableStateOf(1) }
+    var squeezeCount by remember { mutableStateOf(0) }
+
+    val (imageRes, textRes, contentDescRes) = when (etapeActuelle) {
+        1 -> Triple(R.drawable.lemon_tree, R.string.lemon_tree, R.string.lemon_tree_content_description)
+        2 -> Triple(R.drawable.lemon_squeeze, R.string.squeeze, R.string.lemon_content_description)
+        3 -> Triple(R.drawable.lemon_drink, R.string.tap_lemon, R.string.lemonade_content_description)
+        else -> Triple(R.drawable.lemon_restart, R.string.empty_glass, R.string.empty_glass_content_description)
+    }
 }
 
 @Preview(showBackground = true)
